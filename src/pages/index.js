13e71ef283af9c26549ad8e769f1de409e69165d
Sung Kim <sungmkim80@gmail.com>
2018-03-31 11:50:52 -0400

    a minor change for CD

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from "gatsby-link";
 export default () => (
   <div style={{ color: "tomato" }}>
     <h1>Hello Gatsby!</h1>
-    <p>What a world!!!</p>
+    <p>Gatsby look awesome!</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
     <br />
     <div>
